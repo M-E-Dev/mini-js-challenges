7582579a3492be0bc7d2c148920a6592117c030a
Muhammed Eroğlu <llofmejb@gmail.com>
2022-02-02 16:45:18 +0300

    2

--- 01-fibonacci/app.js
+++ 01-fibonacci/app.js
@@ -1,9 +1,9 @@
 const sayıHtml = document.querySelector("#sayı");
 // sayıHtml.addEventListener("keyup", ()=>{sayı = sayıHtml.value})
-
 let degerHtml = document.querySelector("#deger");
-
 const funcBtn = document.querySelector("#start")
+
+
 
 funcBtn.addEventListener("click", function() {
     sayı = sayıHtml.value;
@@ -18,24 +18,6 @@ funcBtn.addEventListener("click", function() {
         num2 = deger;
     }
     console.log(deger);
-    degerHtml.innerHTML = `${sayı} sayısının fibonacci değeri = ${deger}`;
+    degerHtml.innerHTML = `${sayı}. sıradaki fibonacci değeri = ${deger}`;
     return deger;
 });
-
-
-
-
-
-
-// function fibonacci(n) {
-//     let num1 = 0;
-//     let num2 = 1;
-//     let deger = 0;
-//     for (let i = 0; i <= n; i++) {
-//         deger = num1 + num2;
-//         num1 = num2;
-//         num2 = deger;
-//     }
-//     return deger;
-// }
-
